2a845e902d2e268920133750dcc0e9149f576000
tonfi filho <oficinadosdrones@gmail.com>
2025-05-17 15:04:07 +0100

    fixing project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/_header/header.component";
 import { FooterComponent } from "./components/footer/footer.component";
+import { MainComponent } from "./components/main/main.component";
 
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, MainComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
