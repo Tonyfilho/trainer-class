354c709555bb83426390201beef0acfe01b0c254
Tonyfilho <oficinadosdrones@gmail.com>
2025-11-06 16:23:24 +0000

    chaves

---
+++ src/environments/environment.keys.ts
@@ -0,0 +1,5 @@
+export const  keys = {
+  firebaseApiKey: "AIzaSyAxV_-8tHsrolEod_YujzL1oRvXOFIoBas",
+  weatherKey: "10c1f9f3b188464eb7f183243251705",
+  reqresKey: "reqres-free-v1"
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,16 @@
+import { keys } from "./environment.keys";
+
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: keys.firebaseApiKey,
+    authDomain: "ebook-angular-dev-hit.firebaseapp.com",
+    projectId: "ebook-angular-dev-hit",
+    storageBucket: "ebook-angular-dev-hit.firebasestorage.app",
+    messagingSenderId: "576636567121",
+    appId: "1:576636567121:web:559d4455700fc9bdc74270"
+  },
+  weatherKey: keys.weatherKey,
+  reqresKey: keys.reqresKey
+
+}
