4b888a0f9199bb10a9c9e292be381b42f4bdf050
Tonyfilho <oficinadosdrones@gmail.com>
2025-11-06 12:42:25 +0000

    update angular.json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { CommonModule } from '@angular/common';
+
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./components/_header/header.component";
 import { FooterComponent } from "./components/footer/footer.component";
@@ -7,7 +7,7 @@ import { MainComponent } from "./components/main/main.component";
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, HeaderComponent, FooterComponent, MainComponent],
+  imports: [HeaderComponent, FooterComponent, MainComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
