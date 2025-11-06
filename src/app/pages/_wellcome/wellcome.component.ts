445340f1e6dd08d31350c551496733ea669740e9
Tonyfilho <oficinadosdrones@gmail.com>
2025-11-06 14:24:24 +0000

    update

--- src/app/pages/_wellcome/wellcome.component.ts
+++ src/app/pages/_wellcome/wellcome.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 
 @Component({
   selector: 'app-wellcome',
   imports: [],
   templateUrl: './wellcome.component.html',
-  styleUrl: './wellcome.component.css'
+  styleUrl: './wellcome.component.css',
 })
 export class WellcomeComponent {
+  courseCode = signal('UC... or UFCD...');
+  trainerName = signal('Tony');
 
+  // Computed signal - atualiza automaticamente quando courseCode ou trainerName mudam
+  welcomeMessage = computed(
+    () =>
+      `Welcome to ${this.courseCode()} Trainer Class by ${this.trainerName()}`
+  );
+
+  // Exemplo de uso
+  updateCourse(code: string) {
+    this.courseCode.set(code);
+  }
 }
